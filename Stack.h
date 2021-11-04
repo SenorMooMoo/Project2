3cd56a5cac551d4f6649ced7169d4f3a5331a5e9
SenorMooMoo <35905949+SenorMooMoo@users.noreply.github.com>
2021-11-04 17:04:38 -0400

    added documentation

--- Stack.h
+++ Stack.h
@@ -15,43 +15,46 @@ private:
 public:
 
     Stack()= default;
-
+    //add to stack
     void push(char* data){
         stackData.push_back(data);
     }
-
+    //remove and return top of stack
     char* pop(){
         char* data = stackData.at(stackData.size()-1);
         stackData.pop_back();
         return data;
     }
-
+    //return size of stack
     int size(){
         return stackData.size();
     }
-
+    //return top of stack but don't remove from stack
     char* top(){
         return stackData.at(stackData.size()-1);
     }
 
+    //loop through stack and print contents
     void print(){
         for(char* i : stackData){
             std::cout << i << std::endl;
         }
     }
-
+    //set the capacity of the stack only if it is larger
     void setCapacity(size_t capacity){
         if(stackData.capacity() < capacity){
             stackData.reserve(capacity);
         }
     }
-
+    //solve RPN using this stack
     double evaluate(){
+            //this char should always be an operation character because of notation
             char operation = *this->pop();
 
+            //check if there is another operation and if so recursive call
             double operand1 = (!std::isdigit(*this->top())) ? evaluate() : atof(this->pop());
             double operand2 = (!std::isdigit(*this->top())) ? evaluate() : atof(this->pop());
-
+            //convert char operation to operations
             switch (operation) {
                 case '+' :
                     return operand2 + operand1;
--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@
 
 
 int main(int argc, char *argv[]) {
+    //create stack
     Stack stack;
+    //push arguments to stack
     for(int i = 1; i < argc; i++){
         char* a = argv[i];
         stack.push(a);
     }
+    //show stack contents have been properly pushed
     stack.print();
+    //evaluate stack
     std::cout << stack.evaluate() << std::endl;
     return 0;
 }
